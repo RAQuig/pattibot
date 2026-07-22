3cd45e9c018fc512ed356c3959f1c175e0f45b31
Reid Q <92822618+RAQuig@users.noreply.github.com>
2026-07-22 12:44:33 -0600

    Change TARGET_CHANNEL_ID to a list for multiple IDs

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TARGET_CHANNEL_ID = 1448879000863899719  
-
+TARGET_CHANNEL_ID = [
+    1448879000863899719,
+    1280618686553526303
+]
+    
 PATTI_GIFS = [
     "https://tenor.com/view/hrithik-roshan-fighter-teaser-bollywood-teaser-vande-mataram-sujalam-sufalam-gif-4951463871021573191"
 ]
@@ -22,7 +25,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.id == TARGET_CHANNEL_ID:
+    if message.channel.id in TARGET_CHANNEL_ID:
         await message.channel.send("Yeah but have you seen Patti?")
         for gif in PATTI_GIFS:
             await message.channel.send(gif)
